274f15123718243fc3170a73a54a399b6c981483
Pawel Bozejewski <bozejewski@gmail.com>
2020-05-19 12:59:10 +0200

    update

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,38 +2,55 @@ import React, {Component} from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
+        value: this.props.value,
         tags: []
     };
 
-    renderTags() {
-        if (this.state.tags.length === 0) return <h1>Lista jest pusta</h1>;
+    // renderTags() {
+    //     if (this.state.tags.length === 0) return <h1>Lista jest pusta</h1>;
+    //
+    //     return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+    //
+    // }
 
-        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
-
+    handleIncrement = () => {
+        this.setState({value: this.state.value + 1});
     }
 
     render() {
+
         return (
             <>
-                {/*<span className={this.getBadgeClasses()}>{this.formatCount()}</span>*/}
-                {/*<button className="btn btn-secondary btn-sm">Increment</button>*/}
-                {this.state.tags.length ===0 && "Twoja stara"}
-                {this.renderTags()}
+                <h3>{this.props.id}</h3>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button
+                    className="btn btn-secondary btn-sm"
+                    onClick={this.handleIncrement}
+                >
+                    Increment
+                </button>
+                <button
+                    className="btn btn-danger btn-sm m-2"
+                    onClick={() => this.props.onDelete(this.props.id)}
+                >
+                    Delete
+                </button>
+                {/*{this.state.tags.length === 0 && "Test "}*/}
+                {/*{this.renderTags()}*/}
             </>
         );
     }
 
-    // getBadgeClasses() {
-    //     let classes = "badge m-2 badge-";
-    //     classes += (this.state.count === 0 ? "warning" : "primary");
-    //     return classes;
-    // }
-    //
-    // formatCount() {
-    //     const {count} = this.state
-    //     return count === 0 ? 'Zero' : count;
-    // }
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.value === 0 ? "warning" : "primary");
+        return classes;
+    }
+
+    formatCount() {
+        const {value} = this.state
+        return value === 0 ? 'Zero' : value;
+    }
 }
 
 export default Counter;
